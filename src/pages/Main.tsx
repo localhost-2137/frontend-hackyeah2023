a894c3b214d5e3a328e15773a1d6448000748c50
Maksymilian Gala <94986692+maxidragon@users.noreply.github.com>
2023-09-30 19:49:45 +0200

    Add i18n (#1)

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,18 +1,24 @@
 import { motion } from "framer-motion"
+import { useTranslation } from 'react-i18next';
+import LanguageSwitcher from "../components/LanguageSwitcher";
 
 const Main = () => {
-    return (
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        exit={{ opacity: 0, transition: { duration: 0.5 } }}
-      >
-        <header>
-          <h1>EduSearch</h1>
-        </header>
-        <main></main>
-      </motion.div>
-    );
+  const { t } = useTranslation();
+
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0, transition: { duration: 0.5 } }}
+    >
+      <header>
+        <h1>EduSearch</h1>
+      </header>
+      <main>
+        <LanguageSwitcher />
+        <h1>{t('welcome')}</h1></main>
+    </motion.div>
+  );
 }
 
 export default Main
