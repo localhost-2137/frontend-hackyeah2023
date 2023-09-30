279ad381cbb54477c29921208d64a6997110d586
Tomasz Mamala <tomaszmamala@gmail.com>
2023-10-01 01:36:10 +0200

    Small changes

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,14 +1,20 @@
 import Filters from "../components/Filters.tsx";
 import AcademyList from "../components/AcademyList.tsx";
+import { motion } from "framer-motion";
 
 const Search = () => {
     return (
-        <div className="flex w-full max-w-[1300px] mx-auto my-6 px-6 gap-10">
-            <Filters/>
-            <div className="w-[2px] min-h-full bg-dark-300"></div>
-            <AcademyList/>
-        </div>
-    )
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0, transition: { duration: 0.5 } }}
+        className="flex w-full max-w-[1300px] mx-auto my-6 px-6 gap-10"
+      >
+        <Filters />
+        <div className="w-[2px] min-h-full bg-dark-300"></div>
+        <AcademyList />
+      </motion.div>
+    );
 };
 
 export default Search;
