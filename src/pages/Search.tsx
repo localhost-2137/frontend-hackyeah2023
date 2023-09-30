cca7d58a740ebed33f13c06a7f0c1c59eedb61f9
maxidragon <mgwspinanie2006@wp.pl>
2023-09-30 21:47:56 +0200

    up

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,3 +1,4 @@
+import Input from "../components/ui/Input";
 import Tag from "../components/ui/Tag";
 
 const Search = () => {
@@ -5,9 +6,14 @@ const Search = () => {
         <>
             <div className="flex h-screen">
                 <div className="h-full w-30%">
+                    <div className={""}>
+                        <h5 className={"text-2xl font-bold"}>Localization</h5>
+                        <Input type="text" placeholder="Search by city" />
+                        <Tag text="test" />
+                    </div>
                     <input placeholder="test"></input>
                     <div className={"flex flex-col gap-2"}>
-                        <Tag text="test" />
+                      
                     </div>
                 </div>
                 <div className="h-full flex-1">
