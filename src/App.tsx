bbf014dabe87b999d84854e275446bb1b4585c9b
Tomasz Mamala <tomaszmamala@gmail.com>
2023-09-30 19:36:04 +0200

    Added Suvey landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,4 @@
 import {useLocation, useRoutes} from "react-router-dom";
-<<<<<<< HEAD
-import MainDashboard from "./pages/MainDashboard.tsx";
-import {AnimatePresence} from "framer-motion";
-import {cloneElement} from "react";
-import IndexInfo from "./components/IndexInfo.tsx";
-
-const App = () => {
-    const element = useRoutes([
-        {
-            path: "/",
-            element: <MainDashboard/>,
-            children: [
-                {path: "/", element: <IndexInfo/>},
-                {path: "/search", element: <p>Search</p>},
-            ]
-        },
-=======
 import Main from "./pages/Main.tsx";
 import { AnimatePresence } from "framer-motion";
 import { cloneElement } from "react";
@@ -31,7 +14,6 @@ const App = () => {
         path: "/survey",
         element: <SurveyLandingPage />
       }
->>>>>>> fea6ead (Added Suvey landing page)
     ]);
 
     const location = useLocation();
