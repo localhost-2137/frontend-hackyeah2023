e16dcdc46ebac35404cf2ef752c8158a9d7b0029
Tomasz Mamala <tomaszmamala@gmail.com>
2023-09-30 23:38:03 +0200

    Added language translates into landing page

--- src/components/IndexInfo.tsx
+++ src/components/IndexInfo.tsx
@@ -5,19 +5,27 @@ const IndexInfo = () => {
     const { t } = useTranslation();
 
     return (
-        <main className="flex flex-col-reverse md:flex-row gap-32 p-10 justify-between items-center text-center text-lg mt-10 max h-full">
-            <img src="/logo.png" alt="EduSearch Logo" className="w-96 md:hidden mb-5" />
-            <img src="/logo.png" alt="EduSearch Logo" className="w-96 hidden md:block" />
-            <div className="flex flex-col w-128 gap-5 ml-10">
-                <h1 className="text-5xl">EduSearch</h1>
-                <p className="max-w-[400px] text-2xl">Feeling indecisive? Don't worry, we're here to help you find your path. Let's explore your passions and goals together for a confident educational journey. </p>
-                <p className="max-w-[400px]">Click this button and take short survey, which would help you decide.</p>
-                <Button variant="green" isLink={true} to="/survey">{t("surveyLandingPageButton")}</Button>
-            </div>
-
-        </main>
-
-    )
+      <main className="flex flex-col-reverse md:flex-row gap-32 p-10 justify-between items-center text-center text-lg max h-full">
+        <img
+          src="/logo.png"
+          alt="EduSearch Logo"
+          className="w-96 md:hidden mb-5"
+        />
+        <img
+          src="/logo.png"
+          alt="EduSearch Logo"
+          className="w-96 hidden md:block"
+        />
+        <div className="flex flex-col w-128 gap-5 ml-10">
+          <h1 className="text-5xl">EduSearch</h1>
+          <p className="max-w-[400px] text-xl">{t("index-info-1")}</p>
+          <p className="max-w-[400px] text-xl">{t("index-info-2")}</p>
+          <Button variant="green" isLink={true} to="/survey">
+            {t("surveyLandingPageButton")}
+          </Button>
+        </div>
+      </main>
+    );
 }
 
 export default IndexInfo
--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -5,24 +5,30 @@ import LanguageSwitcher from "../components/LanguageSwitcher";
 const Main = () => {
 
     return (
-        <motion.div
-            initial={{opacity: 0}}
-            animate={{opacity: 1}}
-            exit={{opacity: 0, transition: {duration: 0.5}}}
-            className={"flex flex-col items-center h-screen w-full"}
-        >
-            <header className="w-full max-w-[1300px] p-6 flex justify-between items-center">
-                <h1 className="text-4xl">EduSearch</h1>
-                <div className="flex gap-5 items-center">
-                    <Link to="/search" className="text-3xl text-add3-300 hover:text-add3-500">Search</Link>
-                    <LanguageSwitcher/>
-                </div>
-
-            </header>
-            <main className="flex-1">
-                <Outlet/>
-            </main>
-        </motion.div>
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0, transition: { duration: 0.5 } }}
+        className={"flex flex-col items-center h-screen w-full"}
+      >
+        <header className="w-full max-w-[1300px] p-6 flex justify-between items-center">
+          <Link to="/">
+            <h1 className="text-4xl">EduSearch</h1>
+          </Link>
+          <div className="flex gap-5 items-center">
+            <Link
+              to="/search"
+              className="text-3xl text-add3-300 hover:text-add3-500"
+            >
+              Search
+            </Link>
+            <LanguageSwitcher />
+          </div>
+        </header>
+        <main className="flex-1">
+          <Outlet />
+        </main>
+      </motion.div>
     );
 }
 
