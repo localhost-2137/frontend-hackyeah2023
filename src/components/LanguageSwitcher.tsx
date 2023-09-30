36536ffdbfb3d5f0e6176fc1b725bd1652e306ff
maxidragon <mgwspinanie2006@wp.pl>
2023-09-30 19:49:15 +0200

    Add i18n

---
+++ src/components/LanguageSwitcher.tsx
@@ -0,0 +1,18 @@
+import { useTranslation } from 'react-i18next';
+
+function LanguageSwitcher() {
+  const { i18n } = useTranslation();
+
+  const changeLanguage = (lng: any) => {
+    i18n.changeLanguage(lng);
+  };
+
+  return (
+    <div>
+      <button onClick={() => changeLanguage('en')}>English</button>
+      <button onClick={() => changeLanguage('pl')}>Polski</button>
+    </div>
+  );
+}
+
+export default LanguageSwitcher;
