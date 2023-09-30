50879fb395fc68fd14711ec642210bd40f951913
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-09-30 19:38:55 +0200

    add styles to IndexInfo.tsx

--- src/components/IndexInfo.tsx
+++ src/components/IndexInfo.tsx
@@ -1,10 +1,11 @@
 const IndexInfo = () => {
     return (
-        <main>
-            <h2 className="text-xl">EduSearch</h2>
-            <p>Feeling indecisive? Don't worry, we're here to help you find your path. Let's explore your passions and
+        <main className="h-full flex gap-4 flex-col justify-center items-center text-center text-lg">
+            <h2 className="text-3xl">EduSearch</h2>
+            <p className="max-w-[400px]">Feeling indecisive? Don't worry, we're here to help you find your path. Let's
+                explore your passions and
                 goals together for a confident educational journey. </p>
-            <p>Click this button and take short survey, which would help you decide.</p>
+            <p className="max-w-[400px]">Click this button and take short survey, which would help you decide.</p>
         </main>
     )
 }
