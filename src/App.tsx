aae27d75042d82855c666f77966e7ef8a62bf023
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-09-30 19:27:42 +0200

    add Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import {useLocation, useRoutes} from "react-router-dom";
-import Main from "./pages/Main.tsx";
-import { AnimatePresence } from "framer-motion";
-import { cloneElement } from "react";
+import MainDashboard from "./pages/MainDashboard.tsx";
+import {AnimatePresence} from "framer-motion";
+import {cloneElement} from "react";
 
 const App = () => {
     const element = useRoutes([
-      {
-        path: "/",
-        element: <Main />,
-      },
+        {
+            path: "/",
+            element: <MainDashboard/>,
+        },
     ]);
 
     const location = useLocation();
@@ -16,9 +16,9 @@ const App = () => {
     if (!element) return null;
 
     return (
-      <AnimatePresence mode="wait">
-        {cloneElement(element, { key: location.pathname })}
-      </AnimatePresence>
+        <AnimatePresence mode="wait">
+            {cloneElement(element, {key: location.pathname})}
+        </AnimatePresence>
     );
 }
 
---
+++ src/pages/MainDashboard.tsx
@@ -0,0 +1,22 @@
+import {motion} from "framer-motion";
+import {Link, Outlet} from "react-router-dom";
+
+const MainDashboard = () => {
+    return (
+        <motion.div initial={{opacity: 0}}
+                    animate={{opacity: 1}}
+                    exit={{opacity: 0, transition: {duration: 0.5}}}
+                    className="flex flex-col h-screen items-center "
+        >
+            <header className="p-6 w-full max-w-[1300px] mx-6 flex justify-between items-center">
+                <h1 className="text-4xl">EduSearch</h1>
+                <Link className="text-3xl text-add3-300 hover:text-add3-500" to="/search">Search</Link>
+            </header>
+            <main>
+                <Outlet/>
+            </main>
+        </motion.div>
+    )
+}
+
+export default MainDashboard
