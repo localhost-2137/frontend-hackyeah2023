823a4577d886de8ce5c9995f59bfa03280943713
Tomasz Mamala <tomaszmamala@gmail.com>
2023-09-30 22:05:33 +0200

    Added survey in both languages, which saves data in surveyAnswersAtom (#5)

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 type ButtonProps = {
+
     children: React.ReactNode;
     type?: "button" | "submit" | "reset";
     className?: string;
