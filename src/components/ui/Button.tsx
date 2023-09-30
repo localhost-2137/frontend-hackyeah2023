7e881e490b1477e142d40626fdfb3434eea19cb9
Tomasz Mamala <tomaszmamala@gmail.com>
2023-09-30 20:39:25 +0200

    Added isLink property to button

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,17 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 type ButtonProps = {
-    children: React.ReactNode,
-    type?: "button" | "submit" | "reset",
-    className?: string
-}
+  children: React.ReactNode;
+  type?: "button" | "submit" | "reset";
+  className?: string;
+  isLink?: boolean;
+  to?: string;
+};
 
-const Button = ({children, type, className}: ButtonProps) => {
+const Button = ({ children, type, className, isLink, to }: ButtonProps) => {
+  if (isLink && to) {
     return (
-        <button type={type} className={`border-none px-4 py-5 ${className}`}>
-            {children}
-        </button>
-    )
-}
+      <Link to={to} className={`border-none px-4 py-5 ${className}`}>
+        {children}
+      </Link>
+    );
+  } else
+    return (
+      <button type={type} className={`border-none px-4 py-5 ${className}`}>
+        {children}
+      </button>
+    );
+};
 
-export default Button
+export default Button;
--- src/pages/Survey/SurveyLandingPage.tsx
+++ src/pages/Survey/SurveyLandingPage.tsx
@@ -1,5 +1,6 @@
 import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
+import Button from "../../components/ui/Button";
 
 export default function SurveyLandingPage() {
     const { t } = useTranslation();
@@ -9,9 +10,10 @@ export default function SurveyLandingPage() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        className="flex flex-col items-center justify-center h-screen"
+        className="flex flex-col items-center justify-center h-screen px-4"
       >
         <p className="text-center text-4xl">{t("surveyDescription")}</p>
+        <Button className="mt-10">{t("surveyLandingPageButton")}</Button>
       </motion.div>
     );
 }
