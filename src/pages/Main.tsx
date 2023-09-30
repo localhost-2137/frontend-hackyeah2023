1d50b1e72bdd0f85e67e25fcbd900b05133d3141
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-09-30 19:11:24 +0200

    add header

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,6 +1,13 @@
 const Main = () => {
     return (
-        <h1 className="text-2xl">Hello world</h1>
+        <>
+            <header>
+                <h1>EduSearch</h1>
+            </header>
+            <main>
+
+            </main>
+        </>
     )
 }
 
