535d21a8d59e827f27befae298942d6cf3b7e7ba
Tomasz Mamala <tomaszmamala@gmail.com>
2023-09-30 20:31:44 +0200

    idk

--- src/i18n.ts
+++ src/i18n.ts
@@ -16,7 +16,7 @@ i18n
   .use(initReactI18next)
   .init({
     resources,
-    lng: 'en', 
+    lng: 'pl', 
     keySeparator: false, 
     interpolation: {
       escapeValue: false,
--- src/pages/Survey/SurveyLandingPage.tsx
+++ src/pages/Survey/SurveyLandingPage.tsx
@@ -1,8 +1,17 @@
 import { motion } from "framer-motion";
+import { useTranslation } from "react-i18next";
 
 export default function SurveyLandingPage() {
+    const { t } = useTranslation();
 
-    return <motion.div>
-        <p></p>
-    </motion.div>
+    return (
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        className="flex flex-col items-center justify-center h-screen"
+      >
+        <p className="text-center text-4xl">{t("surveyDescription")}</p>
+      </motion.div>
+    );
 }
