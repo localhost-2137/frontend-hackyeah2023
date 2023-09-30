a79c1c6ad250a5dc897b88ba775b6902f76af809
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-09-30 20:22:28 +0200

    IndexInfo and Search pages

--- src/components/IndexInfo.tsx
+++ src/components/IndexInfo.tsx
@@ -1,4 +1,8 @@
+import {useTranslation} from "react-i18next";
+
 const IndexInfo = () => {
+    const {t} = useTranslation();
+
     return (
         <main className="h-full flex gap-4 flex-col justify-center items-center text-center text-lg">
             <h2 className="text-3xl">EduSearch</h2>
