1f1f4a9e7b28eeaab9cc9c2a3ec6c549e3749802
maxidragon <mgwspinanie2006@wp.pl>
2023-09-30 21:14:36 +0200

    Add button variations

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -7,21 +7,35 @@ type ButtonProps = {
   className?: string;
   isLink?: boolean;
   to?: string;
+  variant?: string
 };
 
-const Button = ({ children, type, className, isLink, to }: ButtonProps) => {
+const Button = ({ children, type, className, isLink, to, variant }: ButtonProps) => {
+  let classes = `border-none px-4 py-5`;
+  if (className) {
+    classes += ` ${className}`;
+  }
   if (isLink && to) {
     return (
-      <Link to={to} className={`border-none px-4 py-5 ${className}`}>
+      <Link to={to} className={`border-none px-4 py-5 ${className} `}>
         {children}
       </Link>
     );
-  } else
-    return (
-      <button type={type} className={`border-none px-4 py-5 ${className}`}>
-        {children}
-      </button>
-    );
+  } else {
+    if (variant === "light-blue") {
+      classes += "bg-light-blue-500 hover:bg-light-blue-600 text-white";
+    } else if (variant === "red") {
+      classes += "bg-red-500 hover:bg-red-600 text-white";
+    } else if (variant === "white") {
+      classes += "bg-white hover:bg-gray-100 text-gray-900";
+    }
+    console.log(variant, classes);
+  }
+  return (
+    <button type={type} className={classes}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
