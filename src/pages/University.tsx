45f234bfbc8d5cfb6b72f1d102e8c1455b56b45e
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-10-01 02:40:53 +0200

    add translate to university page

--- src/pages/University.tsx
+++ src/pages/University.tsx
@@ -1,9 +1,11 @@
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import Map from "../components/Map";
+import {useTranslation} from "react-i18next";
 
 const University = () => {
 
+    const {t} = useTranslation();
     const [university, setUniversity] = useState([])
     const {id} = useParams()
 
@@ -28,16 +30,15 @@ const University = () => {
         <>
             {university.map(university => {
                 return (
-                    <div className="py-6 max-w-[1300px] w-full flex flex-col gap-6">
+                    <div key={university.id} className="py-6 max-w-[1300px] w-full flex flex-col gap-6">
                         <h2 className="text-2xl">{university.name}</h2>
-                        <div className="flex justify-between items-center">
-                            <p>address: {university.address}</p>
-                            <p>rank: {university.rank}</p>
-                        </div>
+                        <p>{t("university-city")}: {university.city}</p>
+                        <p>{t("university-address")}: {university.address}</p>
+                        <p>{t("university-rank")}: {university.rank}</p>
 
-                        <p>number of students: {university.number_students}</p>
-                        <p>have academic: {university.academic ? "yes" : "no"}</p>
-                        <p>subjects: {university.subjects}</p>
+                        <p>{t("university-students")}: {university.number_students}</p>
+                        <p>{t("university-academic")}: {university.academic ? "yes" : "no"}</p>
+                        <p>{t("university-subjects")}: {university.subjects}</p>
 
                         <Map lat={university.lat} lng={university.lng}/>
                     </div>
