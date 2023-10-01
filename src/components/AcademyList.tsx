b53fb9e4395c887041b0f73bd429fa0c3dae71a8
Tomasz Mamala <tomaszmamala@gmail.com>
2023-10-01 02:12:52 +0200

    Added id to routing universities

--- src/components/AcademyList.tsx
+++ src/components/AcademyList.tsx
@@ -15,7 +15,7 @@ const AcademyList = (props: {
                         animate={{ opacity: 1, transition: { delay: .15 * i } }}
                         className="p-4 border-2 border-dark-300 hover:bg-dark-900 rounded-xl transition-colors duration-300"
                     >
-                        <Link to="/university" className="" key={university.id}>
+                        <Link to={`/university/${university.id}`} className="" key={university.id}>
                             <h4 className="text-xl">{university.name}</h4>
                             <p>address: {university.address}</p>
                             <p>rank: {university.rank}</p>
