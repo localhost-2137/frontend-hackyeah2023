014a4bcd8b68f7333cf79578a3bba4ab31f00241
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-10-01 02:07:00 +0200

    add university details page

--- src/components/AcademyList.tsx
+++ src/components/AcademyList.tsx
@@ -23,7 +23,8 @@ const AcademyList = () => {
     return (
         <div className="flex flex-col justify-center gap-6">
             {universities.map((university) => {
-                return <Link to="/university" className="p-4 border-2 border-dark-300 hover:bg-dark-900"
+                return <Link to={`/university/${university.id}`}
+                             className="p-4 border-2 border-dark-300 hover:bg-dark-900"
                              key={university.id}>
                     <h4 className="text-xl">{university.name}</h4>
                     <p>address: {university.address}</p>
---
+++ src/pages/University.tsx
@@ -0,0 +1,51 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import Map from "../components/Map";
+
+const University = () => {
+
+    const [university, setUniversity] = useState([])
+    const {id} = useParams()
+
+    useEffect(() => {
+
+        const getData = async () => {
+            const response = await fetch("../src/data/universities.json")
+            const data = await response.json()
+
+            if (response.ok) {
+                const result = data.filter(university => university.id == id)
+                console.log(result)
+                setUniversity(result)
+            }
+        }
+
+        getData()
+
+    }, [])
+
+    return (
+        <>
+            {university.map(university => {
+                return (
+                    <div className="py-6 max-w-[1300px] w-full flex flex-col gap-6">
+                        <h2 className="text-2xl">{university.name}</h2>
+                        <div className="flex justify-between items-center">
+                            <p>address: {university.address}</p>
+                            <p>rank: {university.rank}</p>
+                        </div>
+
+                        <p>number of students: {university.number_students}</p>
+                        <p>have academic: {university.academic ? "yes" : "no"}</p>
+                        <p>subjects: {university.subjects}</p>
+
+                        <Map lat={university.lat} lng={university.lng}/>
+                    </div>
+                )
+            })
+            }
+        </>
+    )
+}
+
+export default University
