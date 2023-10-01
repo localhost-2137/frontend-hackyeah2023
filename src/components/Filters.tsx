df51f50bcc3f36af8e79755474390ac5128ada1e
maxidragon <mgwspinanie2006@wp.pl>
2023-10-01 07:46:28 +0200

    Add search courses

--- src/components/Filters.tsx
+++ src/components/Filters.tsx
@@ -7,6 +7,7 @@ import { getAllCities } from "../logic/backend.ts";
 import { formatQueryParams } from "../logic/other.ts";
 import pl from "../locales/pl.json";
 import en from "../locales/en.json";
+import Input from "./ui/Input.tsx";
 
 const Filters = (props: {
     handleSearch: (query: string) => void;
@@ -15,12 +16,13 @@ const Filters = (props: {
     const [cities, setCities] = useState<string[]>([]);
     const [selectedCities, setSelectedCities] = useState<string[]>([]);
     const [selectedCourses, setSelectedCourses] = useState<string[]>([]);
+    const [searchCourse, setSearchCourse] = useState<string>("");
 
     const handleSelect = (selectedOptions: string[]) => {
         setSelectedCities(selectedOptions);
     };
     const handleCourseSelect = (option: string, isChecked: boolean) => {
-        let indexValue= -1;
+        let indexValue = -1;
         if (!isChecked) {
             setSelectedCourses(selectedCourses.filter((course: string) => course !== option));
             return;
@@ -57,49 +59,67 @@ const Filters = (props: {
     }, []);
 
     return (
-      <form
-        onSubmit={handleSearch}
-        className="w-[90%] flex flex-col gap-4 mx-auto mb-8"
-      >
-        <div className="flex gap-4 items-center">
-          <h4>{t("localization")}</h4>
-        </div>
-        <div>
-          <Autocomplete
-            options={cities}
-            onSelect={handleSelect}
-            placeholder={t("city")}
-          />
-        </div>
-        <div className="flex gap-4 items-center mt-3 mb-3 py-4">
-          <h4>{t("coursesStr")}</h4>
-        </div>
-        <div className="flex flex-col items-start gap-4 max-h-[300px] overflow-scroll border-4 border-dark-300 p-4 rounded-xl box-border">
-          {(t("courses", { returnObjects: true }) as any).map(
-            (course: string) => {
-              return (
-                <Checkbox
-                  key={course}
-                  id={course}
-                  label={course}
-                  onChange={(event) => {
-                    handleCourseSelect(course, event.target.checked);
-                  }}
+        <form
+            onSubmit={handleSearch}
+            className="w-[90%] flex flex-col gap-4 mx-auto mb-8"
+        >
+            <div className="flex gap-4 items-center">
+                <h4>{t("localization")}</h4>
+            </div>
+            <div>
+                <Autocomplete
+                    options={cities}
+                    onSelect={handleSelect}
+                    placeholder={t("city")}
                 />
-              );
-            }
-          )}
-        </div>
-        <div className="py-4">
-          <h4>{t("others")}</h4>
-        </div>
-        <div className="flex flex-col items-start mb-4">
-          <Checkbox id="academy" label={t("considerBoardingSchool")} />
-        </div>
-        <Button type="submit" variant="red">
-          {t("search")}
-        </Button>
-      </form>
+            </div>
+            <div className="flex gap-4 items-center mt-3 mb-1 py-4">
+                <h4>{t("coursesStr")}</h4>
+            </div>
+            <div className="flex flex-col items-start mb-4">
+                <Input type="text" value={searchCourse} onChange={(event) => setSearchCourse(event.target.value)} placeholder={t("search")} />
+            </div>
+            <div className="flex flex-col items-start gap-4 max-h-[300px] overflow-scroll border-4 border-dark-300 p-4 rounded-xl box-border">
+                {(t("courses", { returnObjects: true }) as any).map(
+                    (course: string) => {
+                        if (searchCourse !== "") {
+                            if (course.toLowerCase().includes(searchCourse.toLowerCase())) {
+                                return (
+                                    <Checkbox
+                                        key={course}
+                                        id={course}
+                                        label={course}
+                                        onChange={(event) => {
+                                            handleCourseSelect(course, event.target.checked);
+                                        }}
+                                    />
+                                );
+                            }
+                        } else {
+                            return (
+                                <Checkbox
+                                    key={course}
+                                    id={course}
+                                    label={course}
+                                    onChange={(event) => {
+                                        handleCourseSelect(course, event.target.checked);
+                                    }}
+                                />
+                            );
+                        }
+                    }
+                )}
+            </div>
+            <div className="py-4">
+                <h4>{t("others")}</h4>
+            </div>
+            <div className="flex flex-col items-start mb-4">
+                <Checkbox id="academy" label={t("considerBoardingSchool")} />
+            </div>
+            <Button type="submit" variant="red">
+                {t("search")}
+            </Button>
+        </form>
     );
 };
 
